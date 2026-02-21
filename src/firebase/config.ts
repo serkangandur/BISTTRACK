2f5e7097fb453e8b8ca518b3b8e27e938b810e4e
Serkan Gandur <serkangandur@gmail.com>
2026-02-21 12:04:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2259989271-45253",
+  "appId": "1:973532089843:web:5f6fe628cd1bcc333e9686",
+  "apiKey": "AIzaSyAb7Aq672dTFjogzTmC7HUzdw-ocraEwQQ",
+  "authDomain": "studio-2259989271-45253.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "973532089843"
+};
